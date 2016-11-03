45f95476627f07e7978a8a840706a8d9f52dcd0a
bannonco <bannonco@tcd.ie>
2016-11-03 22:06:06 +0000

    Add files via upload
    python client used for sending messages

---
+++ client.py
@@ -0,0 +1,12 @@
+import sys
+import socket
+
+client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+socket_address=('localhost',8000)
+message="hello world"
+
+client_socket.connect(socket_address)
+client_socket.send('GET /echo.php?message=hello world HTTP/1.1\r\n\r\n')
+receive=client_socket.recv(1024)
+client_socket.close()
+print receive
